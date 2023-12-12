eeb8a374a21243291ac8f6873bd891c84ef5de09
Mostafa Kamal <hiremostafa@gmail.com>
2023-12-12 23:58:20 +0600

    scheduler

---
+++ drf_friend/scheduler/schedule.py
@@ -0,0 +1,38 @@
+import inspect
+from .schedule_cron import ScheduleCron
+
+class ScheduleConverter:
+    def __init__(self, schedules):
+        self.schedules = schedules
+        self.result = {}
+
+    def convert(self):
+        for schedule in self.schedules:
+            task_method =  schedule.task_data.get('task')
+            name = schedule.name_data
+            last_part = str(schedule.schedule_cron)
+            task_method_name = task_method.__name__
+            module_name = inspect.getmodule(task_method).__name__
+            
+            self.result[name] = {
+                'task': f'{module_name}.{task_method_name}',
+                'schedule': last_part,
+            }
+
+        return self.result
+
+class Schedule(ScheduleCron):
+    def __init__(self):
+        self.task_data = {}
+        self.name_data = None
+        self.schedule_cron = None
+
+    def task(self, task):
+        self.task_data['task'] = task
+        return self
+
+    def name(self, name):
+        self.name_data = name
+        return self
+
+
---
+++ drf_friend/scheduler/schedule_cron.py
@@ -0,0 +1,163 @@
+
+from celery.schedules import crontab
+
+class ScheduleCron:
+    def __init__(self):
+        self.schedule_cron = None
+
+    def cron(self, cron_schedule):
+        self.schedule_cron = crontab(cron_schedule)
+        return self
+
+    def everySecond(self):
+        self.schedule_cron = crontab('* * * * *')
+        return self
+
+    def everyTwoSeconds(self):
+        self.schedule_cron = crontab('*/2 * * * *')
+        return self
+
+    def everyFiveSeconds(self):
+        self.schedule_cron = crontab('*/5 * * * *')
+        return self
+
+    def everyTenSeconds(self):
+        self.schedule_cron = crontab('*/10 * * * *')
+        return self
+
+    def everyFifteenSeconds(self):
+        self.schedule_cron = crontab('*/15 * * * *')
+        return self
+
+    def everyTwentySeconds(self):
+        self.schedule_cron = crontab('*/20 * * * *')
+        return self
+
+    def everyThirtySeconds(self):
+        self.schedule_cron = crontab('*/30 * * * *')
+        return self
+
+    def everyMinute(self):
+        self.schedule_cron = crontab('* * * * *')
+        return self
+
+    def everyTwoMinutes(self):
+        self.schedule_cron = crontab('*/2 * * * *')
+        return self
+
+    def everyThreeMinutes(self):
+        self.schedule_cron = crontab('*/3 * * * *')
+        return self
+
+    def everyFourMinutes(self):
+        self.schedule_cron = crontab('*/4 * * * *')
+        return self
+
+    def everyFiveMinutes(self):
+        self.schedule_cron = crontab('*/5 * * * *')
+        return self
+
+    def everyTenMinutes(self):
+        self.schedule_cron = crontab('*/10 * * * *')
+        return self
+
+    def everyFifteenMinutes(self):
+        self.schedule_cron = crontab('*/15 * * * *')
+        return self
+
+    def everyThirtyMinutes(self):
+        self.schedule_cron = crontab('*/30 * * * *')
+        return self
+
+    def hourly(self):
+        self.schedule_cron = crontab('0 * * * *')
+        return self
+
+    def hourlyAt(self, minute):
+        self.schedule_cron = crontab(f'{minute} * * * *')
+        return self
+
+    def everyOddHour(self, minutes=0):
+        self.schedule_cron = crontab('1-23/2 * * * *')
+        return self
+
+    def everyTwoHours(self, minutes=0):
+        self.schedule_cron = crontab('*/2 * * * *')
+        return self
+
+    def everyThreeHours(self, minutes=0):
+        self.schedule_cron = crontab('*/3 * * * *')
+        return self
+
+    def everyFourHours(self, minutes=0):
+        self.schedule_cron = crontab('*/4 * * * *')
+        return self
+
+    def everySixHours(self, minutes=0):
+        self.schedule_cron = crontab('*/6 * * * *')
+        return self
+
+    def daily(self):
+        self.schedule_cron = crontab('0 0 * * *')
+        return self
+
+    def dailyAt(self, time):
+        hour, minute = map(int, time.split(':'))
+        self.schedule_cron = crontab(minute=minute, hour=hour)
+        return self
+
+    def twiceDaily(self, first, second):
+        self.schedule_cron = crontab(minute=0, hour=(first, second))
+        return self
+
+    def twiceDailyAt(self, first, second, minute):
+        self.schedule_cron = crontab(minute=minute, hour=(first, second))
+        return self
+
+    def weekly(self):
+        self.schedule_cron = crontab(minute=0, hour=0, day_of_week=0)
+        return self
+
+    def weeklyOn(self, day, time):
+        hour, minute = map(int, time.split(':'))
+        self.schedule_cron = crontab(minute=minute, hour=hour, day_of_week=day)
+        return self
+
+    def monthly(self):
+        self.schedule_cron_cron = crontab(minute=0, hour=0, day_of_month=1)
+        return self
+
+    def monthlyOn(self, day, time):
+        hour, minute = map(int, time.split(':'))
+        self.schedule_cron_cron = crontab(minute=minute, hour=hour, day_of_month=day)
+        return self
+
+    def twiceMonthly(self, first, second, time):
+        hour, minute = map(int, time.split(':'))
+        self.schedule_cron_cron = crontab(minute=minute, hour=hour, day_of_month=(first, second))
+        return self
+
+    def lastDayOfMonth(self, time):
+        hour, minute = map(int, time.split(':'))
+        self.schedule_cron_cron = crontab(minute=minute, hour=hour, day_of_month=-1)
+        return self
+
+    def quarterly(self):
+        self.schedule_cron_cron = crontab(minute=0, hour=0, month_of_year=(1, 4, 7, 10), day_of_month=1)
+        return self
+
+    def quarterlyOn(self, month, time):
+        hour, minute = map(int, time.split(':'))
+        self.schedule_cron_cron = crontab(minute=minute, hour=hour, month_of_year=month, day_of_month=1)
+        return self
+
+    def yearly(self):
+        self.schedule_cron_cron = crontab(minute=0, hour=0, day_of_month=1, month_of_year=1)
+        return self
+
+    def yearlyOn(self, month, day, time):
+        hour, minute = map(int, time.split(':'))
+        self.schedule_cron = crontab(minute=minute, hour=hour, day_of_month=day, month_of_year=month)
+        return self
+
+
