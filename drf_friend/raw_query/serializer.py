8baa7c40315a8b3746df16947be91da1fe3340f2
Mostafa Kamal <hiremostafa@gmail.com>
2024-01-24 22:13:00 +0600

    fix meta

--- drf_friend/raw_query/serializer.py
+++ drf_friend/raw_query/serializer.py
@@ -1,16 +1,21 @@
 from rest_framework import serializers
+from uuid import UUID
+from datetime import datetime, timezone
 
 class RawQuerySerializer(serializers.Serializer):
 
     def generate_fields(self, data):
-        from uuid import UUID
-        from datetime import datetime, timezone
         if not data:
             return []
 
         # Get the first dictionary in the list to determine the fields
         first_data = data[0]
-        the_fields = list(first_data.keys())
+
+        # Check if the 'Meta' attribute exists and has the 'fields' attribute
+        if hasattr(self, 'Meta') and hasattr(self.Meta, 'fields'):
+            the_fields = self.Meta.fields
+        else:
+            the_fields = list(first_data.keys())
 
         for the_field in the_fields:
             # Determine the field type based on the data type in the first dictionary
@@ -36,16 +41,13 @@ class RawQuerySerializer(serializers.Serializer):
             self.fields[the_field] = field_type
 
         return the_fields
-    
- 
+
     def __init__(self, instance=None, data=None, **kwargs):
         super().__init__(instance=instance, data=data, **kwargs)
         self.generate_fields(data)
-        # print(self.get_fields())
-        print(self.get_meta())
-        
-    def get_meta(self):
-        # Check if the 'Meta' attribute exists in the class and has the 'info' attribute
+
+    def get_meta_fields(self):
+        # Check if the 'Meta' attribute exists in the class and has the 'fields' attribute
         if hasattr(self, 'Meta') and hasattr(self.Meta, 'fields'):
             return self.Meta.fields
         else:
